c3bed3048e266bf3064e7e914b01bc2202fc84b4
dani94c <danielacomola@gmail.com>
2020-06-02 00:25:47 +0200

    Eliminati file dopo utilizzo. Aggiunto cambio background lato smartphone per feedback su attività rilevate.

--- wear/src/main/java/it/unipi/covidapp/WearMainActivity.java
+++ wear/src/main/java/it/unipi/covidapp/WearMainActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
-import android.hardware.Sensor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
